9cc92ee51ab1ab4731eb987ddaa18d7ee44e94cd
SavelyLesnikov <166781803+SavelyLesnikov@users.noreply.github.com>
2024-06-03 19:26:20 +0700

    Update homework_operator_overloading.py

--- homework_operator_overloading.py
+++ homework_operator_overloading.py
@@ -3,11 +3,10 @@ class Building:
         self.number_of_floors: int = number_of_floors
         self.building_type: str = building_type
 
-    def __eq__(self, other):
-        return self.number_of_floors == other.building_type
+       def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors and self.building_type == other.building_type
 
 
 b1 = Building(5, 'Industrial')
-b2 = Building(5, 5)
-print(b1 == b1)
-print(b2 == b2)
+b2 = Building(35, 'Office')
+print(b1 == b2)
